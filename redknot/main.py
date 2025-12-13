4d233a99a94015f04a9acdb6e5e76bd0aeb113f8
Sanyam Asthana <sanyam.asthana@gmail.com>
2025-12-13 14:42:14 +0530

    Made the website and pushed to PyPI

--- redknot/main.py
+++ redknot/main.py
@@ -53,7 +53,7 @@ def main():
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>MermaDep Graph</title>
+            <title>RedKnot Graph</title>
         </head>
         <body>
             <pre class="mermaid">
